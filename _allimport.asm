4bfdf25031a4ef28574ce02f0340a6cb49fa991b
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2023-12-02 15:21:23 +0100

    Setting up some files

---
+++ _allimport.asm
@@ -0,0 +1,7 @@
+#importonce
+
+.segment MapData
+* = $4000 "IntroMap"
+  // .import binary "./maps/intro.bin"
+* = $4400 "Level1"
+  // .import binary "./maps/level1.bin"
--- main.asm
+++ main.asm
@@ -1,12 +1,34 @@
 
-#importonce 
+#importonce
+
+.segmentdef Code [start=$1c10]
+.segmentdef MapData [start=$4000]
+
+.file [name="./main.prg", segments="Code, MapData", modify="BasicUpstart", _start=$1c10]
+.file [name="./EpicEscape.prg", segments="Code, MapData", modify="BasicUpstart", _start=$1c10]
+.disk [filename="./EpicEscape.d64", name="EPICESCAPE", id="C2023", showInfo]
+{
+  [name="----------------", type="rel"],
+  [name="--- RAFFAELE ---", type="rel"],
+  [name="--- INTORCIA ---", type="rel"],
+  [name="----------------", type="rel"],
+  [name="EPICESCAPE", type="prg", segments="Code, MapData", modify="BasicUpstart", _start=$1c10],
+  [name="----------------", type="rel"]
+}
 
 c128lib_BasicUpstart128($1c10)
 
 * = $1c10 "Entry"
 Entry: {
-    
+
+    lda #0
+    sta $d020
+    sta $d021
+
+    jsr 27440
+
     rts
 }
 
+#import "_allimport.asm"
 #import "./common/lib/common-global.asm"
